ad724752fbb760ccfa28f0075f954f80291afdd4
Harsh Gupta <harsh.gupta2211@gmail.com>
2025-11-23 00:44:44 -0300

    add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<div>Welcome</div>} />
-      <Route path="/home" element={<div>hello world</div>} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<div>Welcome</div>} />
+            <Route path="/home" element={<div>hello world</div>} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
